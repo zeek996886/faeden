8fdacceaef4d54c5885b83fff49b8c2112133a1f
zeek996886 <zeek996886@gmail.com>
2025-08-03 17:17:02 +0530

    Updated CompanyLogos.tsx

--- src/components/CompanyLogos.tsx
+++ src/components/CompanyLogos.tsx
@@ -2,21 +2,62 @@ import React from 'react';
 
 const CompanyLogos = () => {
   const companies = [
-    { name: 'AMD', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-AMD.png' },
-    { name: 'American Express', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-American-Express.png' },
-    { name: 'Bosch', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654ba70_Client-Bosch.png' },
-    { name: 'Uber', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654ba70_Client-Uber.png' },
-    { name: 'eBay', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654ba70_Client-ebay.png' },
-    { name: 'Lufthansa', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654ba70_Client-Lufthansa.png' },
-    { name: 'Porsche', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-Porsche.png' },
-    { name: 'Personio', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-Personio.png' },
-    { name: 'Vodafone', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-Vodafone.png' },
-    { name: 'Sixt', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-Sixt.png' },
-    { name: 'RedBull', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-Redbull.png' },
-    { name: 'Tommy Hilfiger', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-Tommy-Hilfiger.png' },
-    { name: 'Babbel', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-Babbel.png' },
-    { name: 'Seiko', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-Seiko.png' },
-    { name: 'Perrier', logo: 'https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e6_Client-Pierriere.png' }
+    {
+      name: "AMD",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e458c70e8d765dc3ec_Client-AMD.png",
+    },
+    {
+      name: "Bosch",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654ba707a7773be8149f6a1b_Client-Bosch.png",
+    },
+    {
+      name: "Uber",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654ba707b8baa404a8907797_Client-Uber.png",
+    },
+    {
+      name: "eBay",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654ba7076e3994a2509f8bd6_Client-ebay.png",
+    },
+    {
+      name: "Lufthansa",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654ba7076a090db4dcc19910_Client-lufthansa.png",
+    },
+    {
+      name: "Porsche",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e4517c29fc66589736_Client-Porsche.png",
+    },
+    {
+      name: "Personio",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e4c81aee2b62b83ea6_Client-Personio.png",
+    },
+    {
+      name: "Vodafone",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e4b3664fdd17b98ef3_Client-Vodafone.png",
+    },
+    {
+      name: "Sixt",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e4f5b551303452e5e4_Client-Sixt.png",
+    },
+    {
+      name: "RedBull",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e4d9d0bc4af3378617_Client-RedBull.png",
+    },
+    {
+      name: "Tommy Hilfiger",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e4d8236631fc81cd7e_Client-Tommy-Hilfiger.png",
+    },
+    {
+      name: "Babbel",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e4f74e6a1c57ff7388_Client-Babbel.png",
+    },
+    {
+      name: "Seiko",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e4515b1846176cde22_Client-Seiko.png",
+    },
+    {
+      name: "Perrier",
+      logo: "https://cdn.prod.website-files.com/6364e4e0baec60a3a1eff938/654a11e4892770c6295ea195_Client-Perriere.png",
+    },
   ];
 
   // Duplicate the array for seamless infinite scroll
